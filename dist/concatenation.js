c0e2d601819ee9b3239fe30804de03dabd1f525c
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-10-04 19:17:00 +0300

    hw done

---
+++ dist/concatenation.js
@@ -0,0 +1,6 @@
+function concatenation(firstWord, secondWord) {
+  console.log(`${firstWord}, ${secondWord}`);
+  debugger;
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,8 @@
+function concatenation(firstWord: string, secondWord: string) {
+  console.log(`${firstWord}, ${secondWord}`);
+  debugger;
+  //? зупиняє виконання коду при виклику функції, після лога у консоль,
+  //? у dev tools можна подивитися, які аргументи приймає ф-ція
+}
+
+export { concatenation };
